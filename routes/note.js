1685390050e154646a460517b91e5434f41ee363
Joshua Johnson <joshuatjohnson255@gmail.com>
2022-11-26 00:51:21 +0530

    Added Create note Functionality

--- routes/note.js
+++ routes/note.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controller/note');
 
-router.post('', postController);
+router.post('', postController.postNote);
 
 module.exports = router;
